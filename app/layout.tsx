7c9e23e14f2691f536b1b247583070783d2cc9a2
YALI <MAGNETME1112@GMAIL.COM>
2026-05-22 16:41:06 +0300

    fix layout css build issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,16 @@
 
-import './globals.css'
+import type { Metadata } from "next"
+// import "./globals.css"
+export const metadata: Metadata = {
+  title: "NOVAIR",
+  description: "FEEL THE NEW AIR OF BEAUTY",
+}
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="he" dir="rtl">
       <body>{children}</body>
